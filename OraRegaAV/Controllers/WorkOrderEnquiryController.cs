f0a84970e137bd5ce95d60427111ca5f566a502f
sujayjena <sujay930@gmail.com>
2024-07-09 19:52:25 +0530

    issue fixed

--- OraRegaAV/Controllers/WorkOrderEnquiryController.cs
+++ OraRegaAV/Controllers/WorkOrderEnquiryController.cs
@@ -725,25 +725,31 @@ namespace OraRegaAV.Controllers
                     if (vProductIssuesPhotos_WOEPath != "")
                     {
                         string fileName = db.tblProductIssuesPhotos.Where(x => x.WOEnquiryId == workOrderEnquiry.Id).Select(x => x.PhotoPath).FirstOrDefault();
-                        string vProductIssuesPhotos_WOPath = HttpContext.Current.Server.MapPath("~/Uploads/WorkOrder/ProductIssue/" + workOrderId);
-                        if (!Directory.Exists(vProductIssuesPhotos_WOPath))
+                        if (!string.IsNullOrWhiteSpace(fileName))
                         {
-                            Directory.CreateDirectory(vProductIssuesPhotos_WOPath);
+                            string vProductIssuesPhotos_WOPath = HttpContext.Current.Server.MapPath("~/Uploads/WorkOrder/ProductIssue/" + workOrderId);
+                            if (!Directory.Exists(vProductIssuesPhotos_WOPath))
+                            {
+                                Directory.CreateDirectory(vProductIssuesPhotos_WOPath);
+                            }
+                            File.Copy(Path.Combine(vProductIssuesPhotos_WOEPath, fileName), vProductIssuesPhotos_WOPath + "/" + fileName, true);
                         }
-                        File.Copy(Path.Combine(vProductIssuesPhotos_WOEPath, fileName), vProductIssuesPhotos_WOPath + "/" + fileName, true);
                     }
 
                     //copy the tblPurchaseProofPhotos file to work order folder
-                    string vPurchaseProofPhotos_WOEPath = HttpContext.Current.Server.MapPath("~/Uploads/WOEnquiries/ProductProofs/" + workOrderEnquiry.Id); 
+                    string vPurchaseProofPhotos_WOEPath = HttpContext.Current.Server.MapPath("~/Uploads/WOEnquiries/ProductProofs/" + workOrderEnquiry.Id);
                     if (vPurchaseProofPhotos_WOEPath != "")
                     {
                         string fileName = db.tblPurchaseProofPhotos.Where(x => x.WOEnquiryId == workOrderEnquiry.Id).Select(x => x.PhotoPath).FirstOrDefault();
-                        string vPurchaseProofPhotos_WOPath = HttpContext.Current.Server.MapPath("~/Uploads/WorkOrder/PurchaseProofPhotos/" + workOrderId);
-                        if (!Directory.Exists(vPurchaseProofPhotos_WOPath))
+                        if (!string.IsNullOrWhiteSpace(fileName))
                         {
-                            Directory.CreateDirectory(vPurchaseProofPhotos_WOPath);
+                            string vPurchaseProofPhotos_WOPath = HttpContext.Current.Server.MapPath("~/Uploads/WorkOrder/PurchaseProofPhotos/" + workOrderId);
+                            if (!Directory.Exists(vPurchaseProofPhotos_WOPath))
+                            {
+                                Directory.CreateDirectory(vPurchaseProofPhotos_WOPath);
+                            }
+                            File.Copy(Path.Combine(vPurchaseProofPhotos_WOEPath, fileName), vPurchaseProofPhotos_WOPath + "/" + fileName, true);
                         }
-                        File.Copy(Path.Combine(vPurchaseProofPhotos_WOEPath, fileName), vPurchaseProofPhotos_WOPath + "/" + fileName, true);
                     }
 
                     #region Save Notification
